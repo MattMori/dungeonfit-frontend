1c5aebbc217ba40ef8a53d6165fc70f8941d5426
MattMori <106766621+MattMori@users.noreply.github.com>
2026-06-08 00:33:38 -0300

    fix: import Cronarium layout sync styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
+
 import "./styles/global.css";
+import "./styles/global.layout-fix.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -12,5 +15,5 @@ createRoot(document.getElementById("root")).render(
         <App />
       </AuthProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
